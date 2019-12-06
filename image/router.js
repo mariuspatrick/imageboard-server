3dea65d78ce2256c84b84282c10e8f53071d984b
mariuspatrick <marius.patrick72@gmail.com>
2019-12-06 11:41:46 +0100

    Added image to db

--- image/router.js
+++ image/router.js
@@ -7,4 +7,11 @@ router.get("/image", (req, res) => {
   image.findAll().then(result => res.send(result));
 });
 
+router.post("/image", (req, res) => {
+  image
+    .create(req.body)
+    .then(result => res.send(result))
+    .catch(console.error);
+});
+
 module.exports = router;
--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const db = require("./db");
 const express = require("express");
 const model = require("./image/model");
 const imageRouter = require("./image/router");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-const port = process.env.PORT || 4000;
+const corsMiddleware = cors();
+app.use(corsMiddleware);
+
+const parserMiddleware = bodyParser.json();
+app.use(parserMiddleware);
 
 app.use(imageRouter);
+
+const port = process.env.PORT || 4000;
+
 app.listen(port, console.log(`Listening on port: ${port}`));
